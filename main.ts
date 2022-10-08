c7efb7b5cbb2d6e0aaff6d3357d6d32dc080a9b1
Zauberhut <49160827+Zauberhut@users.noreply.github.com>
2022-10-08 17:51:45 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,86 @@
-grove.onGesture(GroveGesture.Forward, function () {
-	
+grove.onGesture(GroveGesture.Right, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        . . . . #
+        . . . # .
+        . . # . .
+        `)
 })
+grove.onGesture(GroveGesture.Up, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+})
+grove.onGesture(GroveGesture.Down, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
+grove.onGesture(GroveGesture.Forward, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # # . # #
+        # . . . #
+        . . . . .
+        # . . . #
+        # # . # #
+        `)
+})
+grove.onGesture(GroveGesture.Left, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # . . . .
+        . # . . .
+        . . # . .
+        `)
+})
+grove.onGesture(GroveGesture.Backward, function () {
+    basic.showLeds(`
+        # # . # #
+        # . . . #
+        . . . . .
+        # . . . #
+        # # . # #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+})
+grove.initGesture()
 basic.forever(function () {
 	
 })

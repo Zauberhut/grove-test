9743682843f864a40101ff44e307e84467e8a362
Zauberhut <49160827+Zauberhut@users.noreply.github.com>
2022-11-21 13:06:30 +0100

    Sound

--- main.ts
+++ main.ts
@@ -61,6 +61,65 @@ grove.onGesture(GroveGesture.Left, function () {
         . . # . .
         `)
 })
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "rauf") {
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Square,
+        161,
+        3029,
+        255,
+        255,
+        1000,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Curve
+        ), SoundExpressionPlayMode.InBackground)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (receivedString == "runter") {
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Square,
+        3175,
+        72,
+        255,
+        255,
+        1000,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Curve
+        ), SoundExpressionPlayMode.InBackground)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+    } else if (receivedString == "links") {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . . . .
+            . # . . .
+            . . # . .
+            `)
+        music.playMelody("A F E F D G E F ", 400)
+    } else if (receivedString == "rechts") {
+        music.playMelody("G B A G C5 B A B ", 400)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            . . . . #
+            . . . # .
+            . . # . .
+            `)
+    } else {
+        basic.showIcon(IconNames.Ghost)
+    }
+})
 grove.onGesture(GroveGesture.Backward, function () {
     basic.showLeds(`
         # # . # #
@@ -84,5 +143,6 @@ grove.onGesture(GroveGesture.Backward, function () {
         . . . . .
         `)
 })
+music.playTone(262, music.beat(BeatFraction.Whole))
 grove.initGesture()
 radio.setGroup(1)
